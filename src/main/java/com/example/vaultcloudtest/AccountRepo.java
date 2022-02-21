c3df55180d1d25b57a7a5987e245f2652a43ee8d
coldmund <coldmund@gmail.com>
2022-02-21 23:31:53 +0900

    initial code: copy of 'https://github.com/eugenp/tutorials/tree/master/spring-cloud/spring-cloud-vault'

---
+++ src/main/java/com/example/vaultcloudtest/AccountRepo.java
@@ -0,0 +1,10 @@
+package com.example.vaultcloudtest;
+
+import com.example.vaultcloudtest.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepo extends JpaRepository<Account, Long>{
+
+}
---
+++ src/main/java/com/example/vaultcloudtest/AccountResource.java
@@ -0,0 +1,28 @@
+package com.example.vaultcloudtest;
+
+import com.example.vaultcloudtest.domain.Account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountResource {
+    @Autowired
+    private AccountRepo repo;
+
+    @GetMapping("/account/{id}")
+    public ResponseEntity<Account> getAccount(@PathVariable("id") Long id) {
+        
+        Account acc = repo.findById(id).orElse(null);
+        if ( acc != null ) {
+            return new ResponseEntity<Account>(acc, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }        
+    } 
+}
